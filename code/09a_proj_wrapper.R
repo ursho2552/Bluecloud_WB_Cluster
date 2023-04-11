c26c5bad8ceb96911fda0dc6e05659d035933624
alexschickele <alexandre.schickele@univ-cotedazur.fr>
2023-04-11 14:06:19 +0200

    First shot at a projection wrapper function. Structure only.

---
+++ code/09a_proj_wrapper.R
@@ -0,0 +1,42 @@
+#' =============================================================================
+#' @name proj_wrapper
+#' @description wrapper function redirecting towards the projection sub-pipeline
+#' corresponding to the type of data. 
+#' @description In case of proportion data, an input converting section is run,
+#' to properly pass the inputs to python library MBTR
+#' @param QUERY the query object from the master pipeline
+#' @param MODELS the models object from the master pipeline
+#' @param N_BOOTSTRAP number of bootstrap to do for the projections
+#' @param PROJ_PATH (optional) path to a environmental raster, potentially 
+#' different than the one given in the QUERY object. This is the case for 
+#' supplementary projections in other time and climate scenarios for example. 
+#' To your own risk and only for expert users !
+#' @return an updated model list object containing the projections objects
+#' embedded in each model sub-list.
+
+proj_wrapper <- function(QUERY = query,
+                         MODELS = models,
+                         N_BOOTSTRAP = 10,
+                         PROJ_PATH = NULL){
+  
+  # --- 1. Redirection to the presence model projections
+  if(QUERY$CALL$DATA_TYPE == "pres"){
+    # --- 1.1. Load function
+    source(file = paste0(project_wd, "/code/09b_proj_pres.R"))
+    
+    # --- 1.2. Run function
+    m <- proj_pres(QUERY = QUERY,
+                   MODELS = MODELS,
+                   N_BOOTSTRAP = N_BOOTSTRAP,
+                   PROJ_PATH = PROJ_PATH)
+    
+    return(m)
+  } # END if pres
+  
+  # --- 2. Redirection to the continuous model projections
+  
+  
+  # --- 3. Redirection to the proportion model projections
+  
+  
+} # END FUNCTION
