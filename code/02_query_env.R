618e2e5f5d70d4e62498f33aa43bc9114b4c5bee
alexschickele <alexandre.schickele@univ-cotedazur.fr>
2023-04-11 14:05:52 +0200

    Added a re-gridding of biological data on the environmental resolution. In addition, added the possibility for normal extract function above a given number of sample due to the "nearest-non-NA" extraction method being extremely slow for high numbers of samples.

--- code/02_query_env.R
+++ code/02_query_env.R
@@ -7,46 +7,84 @@
 #' @param ENV_VAR vector of names of environmental variables available within
 #' the climatologies available in Blue Cloud. If null all variables are taken.
 #' @param ENV_PATH string, path to the .nc or raster of environmental variables
-#' @return X: a dataframe of environmetal values at the sampling stations and 
+#' @return X: a data frame of environmental values at the sampling stations and 
 #' @return ENV_PATH: the path to the environmental .nc or raster
-
 
 query_env <- function(QUERY_BIO = query,
                       ENV_VAR = NULL,
                       ENV_PATH = "/net/meso/work/aschickele/Diversity/data/features_monthly"){
   
   # =============================== ENV QUERY ==================================
-  # --- 1. Extract sample info
-  # /!\ Depth is not taken into account for now, neither year (1990-2016 = WOA)
-  sample <- QUERY_BIO$S %>% 
-    dplyr::select(decimallongitude, decimallatitude, month)
-  
-  # --- 2. Open features gridded data and names
+  # --- 1. Open features gridded data and names
   # /!\ To change later for a direct query in a .nc file : avoid 2 different raster files
   features <- stack(ENV_PATH) %>% readAll()
   features_name <- stack(paste0(project_wd, "/data/features_mean_from_monthly")) %>% 
     names()
+
+  # --- 2. Re-grid sample on the raster resolution and filter
+  # (1) The cell centers are at .5, thus it is re-gridded to the nearest .5 value
+  # (2) /!\ Depth is not taken into account for now, neither year (1990-2016 = WOA)
+  res <- res(features)[[1]]
+  digit <- nchar(sub('^0+','',sub('\\.','',res)))-1
+  sample <- QUERY_BIO$S %>% 
+    cbind(QUERY_BIO$Y) %>% 
+    mutate(decimallatitude = round(decimallatitude+0.5*res, digits = digit)-0.5*res) %>% 
+    mutate(decimallongitude = round(decimallongitude+0.5*res, digits = digit)-0.5*res) 
   
-  # --- 3. Extract the environmental data in the data frame
+  # --- 3. Select one sample per group of identical coordinates x month
+  # Among each group of identical lat, long and month, one random point is selected
+  # Sample description are the same among each group as we select one worms ID
+  S <- sample %>% 
+    dplyr::select(-measurementvalue) %>% 
+    group_by(decimallongitude, decimallatitude, month) %>% 
+    slice_sample(n = 1) %>% 
+    dplyr::ungroup() 
+  
+  # --- 4. Average measurement value per group of identical coordinates x month
+  Y <- sample %>% 
+    dplyr::select(decimallongitude, decimallatitude, month, measurementvalue) %>% 
+    group_by(decimallongitude, decimallatitude, month) %>% 
+    summarise(measurementvalue = mean(measurementvalue)) %>% 
+    dplyr::ungroup() %>% 
+    dplyr::select(measurementvalue)
+  
+  # --- 5. Extract the environmental data in the data frame
+  # (1) If there is less than 100 points, from nearest non-NA cells
+  # (2) If more than 100 points, uses the simple extract function and discards NA
+  # This choice is motivated by speed requirements and low impact of some NAs on large datasets
   X <- NULL
-  for(j in 1:nrow(sample)){
-    id <- grep(pattern = sample$month[j], names(features))
-    xy <- sample[j,] %>% dplyr::select(x = decimallongitude, y = decimallatitude)
+  for(j in 1:nrow(S)){
+    id <- grep(pattern = S$month[j], names(features))
+    xy <- S[j,] %>% dplyr::select(x = decimallongitude, y = decimallatitude)
     
-    tmp <- mclapply(features[[id]]@layers, function(a_layer) sample_raster_NA(a_layer, xy), mc.cores = 10) %>% 
-      lapply(mean) %>% # average between two points if a coordinate is EXACTLY at an integer value (i.e., between two cells)
-      as.data.frame()
+    if(nrow(S) < 100){
+      tmp <- mclapply(features[[id]]@layers, function(a_layer) sample_raster_NA(a_layer, xy), mc.cores = 10) %>% 
+        lapply(mean) %>% # average between two points if a coordinate is EXACTLY at an integer value (i.e., between two cells)
+        as.data.frame()
+    } else {
+      tmp <- raster::extract(features[[id]], xy) %>% 
+        as.data.frame()
+    } # End if < 100
+
     colnames(tmp) <- features_name
     X <- rbind(X, tmp)
-  }
+  } # End for j
   
-  # --- 4. Write X on the disk
+  # --- 6. Clean NA in S, Y and X
+  to_remove <- which(is.na(apply(X, 1, sum)))
+  S <- S[-to_remove,]
+  X <- X[-to_remove,]
+  Y <- Y[-to_remove,]
+
+  # --- 7. (Re) Write S, Y and X on the disk
+  write_feather(S, paste0(project_wd, "/data/S.feather"))
+  write_feather(Y, paste0(project_wd, "/data/Y.feather"))
   write_feather(X, paste0(project_wd, "/data/X.feather"))
   
-  # --- 5. Append query_bio with the environmental values
-  return(list(Y = QUERY_BIO$Y,
+  # --- 8. Append query_bio with the environmental values
+  return(list(Y = Y,
               X = X,
-              S = QUERY_BIO$S,
+              S = S,
               CALL = list(DATA_TYPE = QUERY_BIO$CALL$DATA_TYPE,
                           SP_SELECT = QUERY_BIO$CALL$SP_SELECT,
                           SAMPLE_SELECT = QUERY_BIO$CALL$SAMPLE_SELECT,
