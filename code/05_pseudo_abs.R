56b8f68409436ec7aa159d52ed34c83de08a8d25
alexschickele <alexandre.schickele@univ-cotedazur.fr>
2023-09-13 13:57:02 +0200

    Additional plots including histogram of observed data; longitude and latitude spectrums.

--- code/05_pseudo_abs.R
+++ code/05_pseudo_abs.R
@@ -23,33 +23,34 @@ pseudo_abs <- function(FOLDER_NAME = NULL,
   load(paste0(project_wd, "/output/", FOLDER_NAME,"/", SUBFOLDER_NAME, "/QUERY.RData"))
   if(is.null(CALL$NB_PA)){CALL$NB_PA = nrow(QUERY$S)}
   
-  # --- 1.3. Double check data type - plot observation locations anyway
+  # --- 1.3. Base raster and land
+  r <- CALL$ENV_DATA[[1]][[1]]
+  r[!is.na(r)] <- 0
+  
+  land <- r
+  land[is.na(land)] <- 9999
+  land[land != 9999] <- NA
+  
+  # --- 2. Double check data type - plot observation locations anyway
   if(CALL$DATA_TYPE != "binary"){
     message("No Pseudo-absence generation necessary for this data type")
-    r <- CALL$ENV_DATA[[1]][[1]]
-    r[!is.na(r)] <- 0
-    
-    land <- r
-    land[is.na(land)] <- 9999
-    land[land != 9999] <- NA
-    
     pdf(paste0(project_wd,"/output/",FOLDER_NAME,"/",SUBFOLDER_NAME,"/01_observations.pdf"))
+    par(mfrow = c(2,2))
     
-    # --- 1.3.1. Plot for continuous data
+    # --- 2.1.1. Geographical plot for continuous data
     # We plot an artificial land with the scale of the observation first, for the legend
     if(CALL$DATA_TYPE == "continuous"){
       tmp <- (land-9998)*max(QUERY$Y$measurementvalue)
       tmp[1] <- 0
       plot(tmp, col = inferno_pal(100), main = paste("Observation locations for:", SUBFOLDER_NAME), 
            sub = paste("NB_OBS :",  nrow(QUERY$S)))
-      plot(land, col = "antiquewhite4", legend=FALSE, main = paste("Observation locations for:", SUBFOLDER_NAME), 
-           sub = paste("NB_OBS :",  nrow(QUERY$S)), add = TRUE)
+      plot(land, col = "antiquewhite4", legend=FALSE, add = TRUE)
       points(QUERY$S$decimallongitude, QUERY$S$decimallatitude, 
-             col = col_numeric("inferno", domain = range(QUERY$Y$measurementvalue))(QUERY$Y$measurementvalue),
+             col = col_numeric("inferno", domain = range(QUERY$Y$measurementvalue, na.rm = TRUE))(QUERY$Y$measurementvalue),
              pch = 20)
     }
 
-    # --- 1.3.2. Plot for proportion type
+    # --- 2.1.2. Geographical plot for proportion type
     if(CALL$DATA_TYPE == "proportions"){
       plot(land, col = "antiquewhite4", legend=FALSE, main = paste("Observation locations for:", SUBFOLDER_NAME), 
            sub = paste("NB_OBS :",  nrow(QUERY$S)))
@@ -57,15 +58,22 @@ pseudo_abs <- function(FOLDER_NAME = NULL,
              col = "black", pch = 20)
     }
     
+    # --- 2.2. Histogram of values
+    hist(unlist(QUERY$Y), breaks = 25, col = alpha("black", 0.5), main = "Histogram of values", xlab = CALL$DATA_TYPE)
+    
+    # --- 2.3. Longitude and latitude profile
+    hist(QUERY$S$decimallongitude, breaks = seq(-180,180, 10), col = alpha("black", 0.5), 
+         main = "Longitudinal spectrum", xlab = "Longitude")
+    
+    hist(QUERY$S$decimallatitude, breaks = seq(-180,180, 10), col = alpha("black", 0.5), 
+         main = "Latitudinal spectrum", xlab = "Latitude")
+    
+    
     dev.off()
     
     log_sink(FILE = sinkfile, START = FALSE)
     return(NULL)
-  } 
-
-  # --- 2. Load features and create base raster
-  r <- CALL$ENV_DATA[[1]][[1]]
-  r[!is.na(r)] <- 0
+  } # if datatype !binary
   
   # --- 3. Background definition
   # --- 3.1. Based on a geographical distance
@@ -169,13 +177,25 @@ pseudo_abs <- function(FOLDER_NAME = NULL,
   
   # --- 5.4. Fast PDF to check the absences location
   pdf(paste0(project_wd,"/output/",FOLDER_NAME,"/",SUBFOLDER_NAME,"/01_pseudo_abs.pdf"))
-  land <- r
-  land[is.na(land)] <- 9999
-  land[land != 9999] <- NA
-  plot(land, col = "antiquewhite4", legend=FALSE, main = paste("Presence - Pseudo Abs for:", SUBFOLDER_NAME), 
-       sub = paste("NB_OBS :",  nrow(QUERY$S),"// NB_PA :", CALL$NB_PA, "// METHOD_PA :", CALL$METHOD_PA))
-  points(xym[,1:2], col = "red", pch = 3)
-  points(QUERY$S$decimallongitude, QUERY$S$decimallatitude, col = "black", pch = 3)
+  par(mfrow = c(2,2))
+  
+  # --- 5.4.1. Geographical distribution
+  plot(land, col = "antiquewhite4", legend=FALSE, main = paste("Obs. presence for:", SUBFOLDER_NAME), 
+       sub = paste("NB_OBS :",  nrow(QUERY$S)))
+  points(QUERY$S$decimallongitude, QUERY$S$decimallatitude, col = alpha("black", 0.2), pch = 20)
+  
+  plot(land, col = "antiquewhite4", legend=FALSE, main = paste("Pseudo Abs for:", SUBFOLDER_NAME), 
+       sub = paste("NB_PA :", CALL$NB_PA, "// METHOD_PA :", CALL$METHOD_PA))
+  points(xym[,1:2], col = alpha("red", 0.2), pch = 20)
+  
+  # --- 5.4.2. Longitude and latitude profile
+  hist(QUERY$S$decimallongitude, breaks = seq(-180,180, 10), col = alpha("black", 0.5), 
+       main = "Longitudinal spectrum", xlab = "Longitude")
+  hist(xym$x, breaks = seq(-180,180, 10), col = alpha("red", 0.5), add = TRUE)
+  
+  hist(QUERY$S$decimallatitude, breaks = seq(-180,180, 10), col = alpha("black", 0.5), 
+       main = "Latitudinal spectrum", xlab = "Latitude")
+  hist(xym$y, breaks = seq(-180,180, 10), col = alpha("red", 0.5), add = TRUE)
   dev.off()
   
   # --- 6. Append the query
