cfd357ff11354efcf81fcaec16ce0de304614ac6
alexschickele <alexandre.schickele@univ-cotedazur.fr>
2023-09-20 14:01:50 +0200

    New background selection adapted for monthly data. Added supplementary quality check plots

--- code/05_pseudo_abs.R
+++ code/05_pseudo_abs.R
@@ -65,7 +65,7 @@ pseudo_abs <- function(FOLDER_NAME = NULL,
     hist(QUERY$S$decimallongitude, breaks = seq(-180,180, 10), col = alpha("black", 0.5), 
          main = "Longitudinal spectrum", xlab = "Longitude")
     
-    hist(QUERY$S$decimallatitude, breaks = seq(-180,180, 10), col = alpha("black", 0.5), 
+    hist(QUERY$S$decimallatitude, breaks = seq(-90,90, 10), col = alpha("black", 0.5), 
          main = "Latitudinal spectrum", xlab = "Latitude")
     
     
@@ -75,111 +75,120 @@ pseudo_abs <- function(FOLDER_NAME = NULL,
     return(NULL)
   } # if datatype !binary
   
-  # --- 3. Background definition
-  # --- 3.1. Based on a geographical distance
-  # Defining the background as cells distant from more than n-km from presence
-  if(CALL$METHOD_PA == "mindist"){
-    # --- 3.1.1. Extract presence points
-    val <- QUERY$S %>% 
+  # --- 3. Initialize background definition
+  # --- 3.1. Extract the monthly bias
+  # To know how many pseudo-absences to draw from each month background
+  month_freq <- summary(as.factor(QUERY$S$month))
+  month_freq <- trunc(month_freq/(sum(month_freq)/CALL$NB_PA))
+  
+  # --- 3.2. Initialize lon x lat x time df
+  xym <- NULL
+  
+  # --- 4. Extract pseudo-absence
+  for(m in names(month_freq)){
+    # --- 4.1. Extract presence points
+    presence <- QUERY$S %>% 
+      dplyr::select(decimallongitude, decimallatitude, month) %>% 
+      dplyr::filter(month == m) %>% 
       dplyr::select(decimallongitude, decimallatitude)
     
-    # --- 3.1.2. Calculate distance to presences in raster object
-    background <- rasterize(val, r, update=TRUE)
-    background[background < 1] <- NA
-    background <- raster::distance(background)
+    # --- 4.2. Background definition
+    # Need to redefine base raster here in case of different NA between months
+    r <- CALL$ENV_DATA[[as.numeric(m)]][[1]]
+    r[!is.na(r)] <- 0
     
-    background <- synchroniseNA(stack(background, r))[[1]] %>% 
-      rasterToPoints() %>% 
-      as.data.frame() %>% 
-      dplyr::filter(layer < CALL$DIST_PA & !is.na(layer)) %>% 
-      dplyr::select(x, y)
-  } # End if geo
-  
-  # --- 3.2. Random but biased by cumulative-distance to presence
-  if(CALL$METHOD_PA == "cumdist"){
-    # --- 3.2.1. Extract presence points
-    presence <- QUERY$S %>% 
-      dplyr::select(decimallongitude, decimallatitude)
+    # --- 4.2.1. Based on a geographical distance
+    # Defining the background as cells distant from more than n-km from presence
+    if(CALL$METHOD_PA == "mindist"){
+      # Calculate distance to presences in raster object
+      background <- raster::rasterize(presence, r)
+      background[background < 1] <- NA
+      background <- raster::distance(background)
+      
+      # Filter by minimum distance to presence
+      background <- synchroniseNA(stack(background, r))[[1]] %>% 
+        rasterToPoints() %>% 
+        as.data.frame() %>% 
+        dplyr::filter(layer < CALL$DIST_PA & !is.na(layer)) %>% 
+        dplyr::select(x, y)
+    } # End if mindist
     
-    # --- 3.2.2. Compute cumulative distance
-    background <- r %>% rasterToPoints() %>% .[,1:2]
-    cumdist <- pointDistance(presence, background, lonlat = TRUE) %>% 
-      apply(2, sum)
-    
-    # --- 3.2.3. Define weighted background raster
-    background <- r
-    background[!is.na(background)] <- cumdist
-    background <- (background/max(getValues(background), na.rm = TRUE)-1)*-1
-    
-    background <- synchroniseNA(stack(background, r))[[1]] %>% 
-      rasterToPoints() %>% 
-      as.data.frame()
-  } # End if bias_env
-  
-  # --- 3.3. Density of presence within a buffer
-  if(CALL$METHOD_PA == "density"){
-    # --- 3.3.1. Extract presence points
-    presence <- QUERY$S %>% 
-      dplyr::select(decimallongitude, decimallatitude) %>% 
-      rasterize(r)
-    presence[!is.na(presence)] <- 1
-    
-    # --- 3.3.2. Compute density
-    focal_w <- focalWeight(r, 20, type = "Gauss")
-    dens <- focal(presence, focal_w, fun = function(x){sum(x, na.rm = TRUE)}, pad = TRUE)
-    dens <- (dens/max(getValues(dens), na.rm = TRUE)*(1-CALL$PER_RANDOM))+CALL$PER_RANDOM # try to get a fix random PA generation
-    dens[!is.na(presence)] <- NA
-    
-    # --- 3.3.3. Define the weighted background raster
-    background <- synchroniseNA(stack(dens, r))[[1]] %>% 
-      rasterToPoints() %>% 
-      as.data.frame()
-  } # End if density
-  
-  # --- 4. Additional background filter
-  if(!is.null(CALL$BACKGROUND_FILTER)){
-    if(is.data.frame(CALL$BACKGROUND_FILTER)){
-      background <- CALL$BACKGROUND_FILTER
-    } else {
-      background <- raster(CALL$BACKGROUND_FILTER) %>% 
+    # --- 4.2.2. Biased by cumulative-distance to presence
+    if(CALL$METHOD_PA == "cumdist"){
+      
+      # Compute cumulative distance
+      background <- r %>% rasterToPoints() %>% .[,1:2]
+      cumdist <- pointDistance(presence, background, lonlat = TRUE) %>% 
+        apply(2, sum)
+      
+      # Define weighted background raster
+      background <- r
+      background[!is.na(background)] <- cumdist
+      background <- (background/max(getValues(background), na.rm = TRUE)-1)*-1
+      
+      background <- synchroniseNA(stack(background, r))[[1]] %>% 
         rasterToPoints() %>% 
         as.data.frame()
+    } # End if cumdist
+    
+    # --- 4.2.3. Density of presence within a buffer
+    if(CALL$METHOD_PA == "density"){
+      # Prepare base raster
+      presence <- raster::rasterize(presence, r)
+      presence[!is.na(presence)] <- 1
+      
+      # Compute density
+      focal_w <- focalWeight(r, 20, type = "Gauss")
+      dens <- focal(presence, focal_w, fun = function(x){sum(x, na.rm = TRUE)}, pad = TRUE)
+      dens <- (dens/max(getValues(dens), na.rm = TRUE)*(1-CALL$PER_RANDOM))+CALL$PER_RANDOM # try to get a fix random PA generation
+      dens[!is.na(presence)] <- NA
+      
+      # Define the weighted background raster
+      background <- synchroniseNA(stack(dens, r))[[1]] %>% 
+        rasterToPoints() %>% 
+        as.data.frame()
+    } # End if density
+    
+    # --- 4.2.4. Additional background filter
+    if(!is.null(CALL$BACKGROUND_FILTER)){
+      if(is.data.frame(CALL$BACKGROUND_FILTER)){
+        background <- CALL$BACKGROUND_FILTER
+      } else {
+        background <- raster(CALL$BACKGROUND_FILTER) %>% 
+          rasterToPoints() %>% 
+          as.data.frame()
+      }
+    } # END if background_filter TRUE
+    
+    # --- 4.3. Sample within the background data
+    # Add a resample option if there is not enough background available
+    if(ncol(background == 3)){
+      if(nrow(background) < month_freq[m]){
+        message(" PSEUDO-ABS : background too small, selection with replacement !")
+        tmp <- sample(x = 1:nrow(background), size = month_freq[m], replace = TRUE, prob = background[,3]) # sqr the distance to bias more
+      } else {
+        tmp <- sample(x = 1:nrow(background), size = month_freq[m], prob = background[,3]) # sqr the distance to bias more
+      } 
+    } else {
+      if(nrow(background) < month_freq[m]){
+        message(" PSEUDO-ABS : background too small, selection with replacement !")
+        tmp <- sample(x = 1:nrow(background), size = month_freq[m], replace = TRUE)
+      } else {
+        tmp <- sample(x = 1:nrow(background), size = month_freq[m])
+      } 
     }
-  } # END if background_filter TRUE
-
-  # --- 5. Sample within the background data
-  # --- 5.1. Conditional sampling locations
-  # Add a resample option if there is not enough background available
-  if(ncol(background == 3)){
-    if(nrow(background) < CALL$NB_PA){
-      message(" PSEUDO-ABS : background too small, selection with replacement !")
-      tmp <- sample(x = 1:nrow(background), size = CALL$NB_PA, replace = TRUE, prob = background[,3]**2) # sqr the distance to bias more
-    } else {
-      tmp <- sample(x = 1:nrow(background), size = CALL$NB_PA, prob = background[,3]**2) # sqr the distance to bias more
-    } 
-  } else {
-    if(nrow(background) < CALL$NB_PA){
-      message(" PSEUDO-ABS : background too small, selection with replacement !")
-      tmp <- sample(x = 1:nrow(background), size = CALL$NB_PA, replace = TRUE)
-    } else {
-      tmp <- sample(x = 1:nrow(background), size = CALL$NB_PA)
-    } 
-  }
-
-  # --- 5.2. Sampling month
-  # According to the same distribution as the one in the data
-  month_freq <- summary(as.factor(QUERY$S$month))
-  month_freq <- month_freq/sum(month_freq)
-  month_prob <- sample(x = 1:length(month_freq), size = CALL$NB_PA, replace = TRUE, prob = month_freq)
+    
+    # --- 4.4. Concatenate over month
+    xym0 <- background[tmp,1:2] %>% cbind(rep(as.numeric(m), month_freq[m]))
+    xym <- rbind(xym, xym0)
+    
+  } # m month loop
   
-  # --- 5.2. Subset the background coordinates
-  xym <- background[tmp,1:2] %>% cbind(month_prob)
-  
-  # --- 5.4. Fast PDF to check the absences location
+  # --- 5.Fast PDF to check the absences location
   pdf(paste0(project_wd,"/output/",FOLDER_NAME,"/",SUBFOLDER_NAME,"/01_pseudo_abs.pdf"))
   par(mfrow = c(2,2))
   
-  # --- 5.4.1. Geographical distribution
+  # --- 5.1. Geographical distribution
   plot(land, col = "antiquewhite4", legend=FALSE, main = paste("Obs. presence for:", SUBFOLDER_NAME), 
        sub = paste("NB_OBS :",  nrow(QUERY$S)))
   points(QUERY$S$decimallongitude, QUERY$S$decimallatitude, col = alpha("black", 0.2), pch = 20)
@@ -188,7 +197,7 @@ pseudo_abs <- function(FOLDER_NAME = NULL,
        sub = paste("NB_PA :", CALL$NB_PA, "// METHOD_PA :", CALL$METHOD_PA))
   points(xym[,1:2], col = alpha("red", 0.2), pch = 20)
   
-  # --- 5.4.2. Longitude and latitude profile
+  # --- 5.2. Longitude and latitude profile
   hist(QUERY$S$decimallongitude, breaks = seq(-180,180, 10), col = alpha("black", 0.5), 
        main = "Longitudinal spectrum", xlab = "Longitude")
   hist(xym$x, breaks = seq(-180,180, 10), col = alpha("red", 0.5), add = TRUE)
@@ -216,7 +225,7 @@ pseudo_abs <- function(FOLDER_NAME = NULL,
   # --- 6.3. Sample table
   S <- data.frame(decimallongitude = xym$x,
                   decimallatitude = xym$y,
-                  month = xym$month_prob,
+                  month = xym[,3],
                   measurementtype = "Pseudo-absence") %>% 
     mutate(ID = row_number()+nrow(QUERY$S))
   QUERY$S <- QUERY$S %>% 
